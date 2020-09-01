fe3f740bbf5ce6b2eabe777777ba42f2f6ee0d1e
javier322 <javier.pinto.g@usach.cl>
2020-09-01 10:24:49 -0400

    Fix CORS

--- src/main/scala/com/neurone/neuroneconector/Main.scala
+++ src/main/scala/com/neurone/neuroneconector/Main.scala
@@ -1,6 +1,7 @@
 package com.neurone.neuroneconector
 
 import cats.effect.IO
+import com.twitter.finagle.http.filter.Cors
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Await
@@ -15,16 +16,23 @@ import com.typesafe.config._
 object Main extends App {
 
   //App port
-  val config= ConfigFactory.load()
-  val port= config.getString("app.PORT")
-
-  println("PORT",port)
+  val config = ConfigFactory.load()
+  val port = config.getString("app.PORT")
+  val policy: Cors.Policy = Cors.Policy(
+    allowsOrigin = _ => Some("*"),
+    allowsMethods = _ => Some(Seq("GET", "POST")),
+    allowsHeaders = _ => Some(Seq("Accept"))
+  )
+  println("PORT", port)
 
   //Load all endpoints
-  def service: Service[Request, Response] = Bootstrap
-    .serve[Application.Json](combined)
-    .toService
-    
-  // Init services on port 8081  
-  Await.ready(Http.server.serve(":"+port, service))
+  def service: Service[Request, Response] =
+    Bootstrap
+      .serve[Application.Json](combined)
+      .toService
+
+  val corsService: Service[Request, Response] =
+    new Cors.HttpFilter(policy).andThen(service)
+  // Init services on port 8081
+  Await.ready(Http.server.serve(":" + port, corsService))
 }
